e9f1a7af8f32aeec706544320903338ed9016fad
gwarz <gmwarzecha@tutanota.com>
2021-03-19 16:40:34 -0700

    added post-route, post model, tested endpoints with Insomnia

--- models/index.js
+++ models/index.js
@@ -1,3 +1,14 @@
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+// creates associations between user and posts
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+// defines the relationship of the Post model to the User; post can belong to 1 user, not many
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Post };
