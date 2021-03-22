8a7f0f7c17d1a6beb5ca0a1dbbf83a7b4051bf98
gwarz <gmwarzecha@tutanota.com>
2021-03-21 21:26:37 -0700

    secret

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'cocopuffs',
+  secret: process.env.EXPRESS_SECRET,
   cookie: {
     maxAge: 900000
   },
