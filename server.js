37eed555b5cdebf59896afc3c1057f0c74b7ca62
gwarz <gmwarzecha@tutanota.com>
2021-03-21 21:24:29 -0700

    secret troubleshoot

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: process.env.EXPRESS_SECRET,
+  secret: 'cocopuffs',
   cookie: {
     maxAge: 900000
   },
